6c8e033afe27e174c328f42947eae92bbc6c77e3
Dev Boldly <devboldly@gmail.com>
2020-04-19 11:33:38 -0500

    Improve analytics

--- src/gatsby-theme-docz/components/MainContainer/index.js
+++ src/gatsby-theme-docz/components/MainContainer/index.js
@@ -1,8 +1,20 @@
 import { DoczFooter } from '@devboldly/react-devboldly-tools';
 import { MainContainer as OriginalMainContainer } from 'gatsby-theme-docz/src/components/MainContainer/index';
 import React from 'react';
+import ReactGA from 'react-ga';
+
+let lastPageview = undefined;
 
 export const MainContainer = ({ children, ...rest }) => {
+  // Track page view
+  if (typeof window !== 'undefined') {
+    const newPageview = window.location.pathname + window.location.search;
+    if (newPageview !== lastPageview) {
+      ReactGA.pageview(newPageview);
+      lastPageview = newPageview;
+    }
+  }
+  console.log('children', children);
   return (
     <OriginalMainContainer {...rest}>
       {children}
--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { GoogleAnalytics } from '@devboldly/react-devboldly-tools';
 import { TRACKING_ID } from './analytics-config';
+import ReactGA from 'react-ga';
 
 // See: https://github.com/doczjs/docz/tree/master/core/gatsby-theme-docz#wrapping-the-entire-app
 
-export default ({ children }) => <GoogleAnalytics trackingId={TRACKING_ID}>{children}</GoogleAnalytics>;
+let gaInitialized = false;
+
+export default ({ children }) => {
+  if (!gaInitialized) {
+    ReactGA.initialize(TRACKING_ID);
+    gaInitialized = true;
+  }
+  return children;
+};
