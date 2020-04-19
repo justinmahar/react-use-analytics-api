086adda573869747e8b7d68337e5729c1714d90e
Dev Boldly <devboldly@gmail.com>
2020-04-18 23:06:36 -0500

    Update to use react-devboldly-tools

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,30 +1,7 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { GoogleAnalytics } from '@devboldly/react-devboldly-tools';
 import { TRACKING_ID } from './analytics-config';
 
 // See: https://github.com/doczjs/docz/tree/master/core/gatsby-theme-docz#wrapping-the-entire-app
 
 export default ({ children }) => <GoogleAnalytics trackingId={TRACKING_ID}>{children}</GoogleAnalytics>;
-
-function GoogleAnalytics(props) {
-  return (
-    <>
-      {props.trackingId !== 'UA-123456789-1' && (
-        <Helmet>
-          {/* Global site tag (gtag.js) - Google Analytics */}
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${props.trackingId}`}></script>
-          <script>
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${props.trackingId}');
-        `}
-          </script>
-        </Helmet>
-      )}
-      {props.children}
-    </>
-  );
-}
