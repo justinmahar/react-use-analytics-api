e5bb86bbe28646c7046d7b2be064c6265024a842
Dev Boldly <devboldly@gmail.com>
2020-04-19 18:24:03 -0500

    Check if window is defined

--- src/gatsby-theme-docz/components/MainContainer/index.js
+++ src/gatsby-theme-docz/components/MainContainer/index.js
@@ -7,15 +7,19 @@ export const MainContainer = ({ children, ...rest }) => {
   return (
     <OriginalMainContainer {...rest}>
       {children}
-      <DoczFeedbackForm
-        formActionUrl="https://docs.google.com/forms/u/0/d/e/1FAIpQLScMRAM6r__I_rSeg_l-a5mk8p8iSTDNrjJCUOTlnxa63Czwrg/formResponse"
-        siteName={customThemeConfig.projectName}
-        pageUrl={window.location.href}
-        yesNoFieldName="entry.681938809"
-        feedbackFieldName="entry.593370879"
-        siteFieldName="entry.1824430892"
-        pageUrlFieldName="entry.2081371977"
-      />
+      {typeof window !== 'undefined' &&
+        window.location.pathname !== '/' &&
+        window.location.pathname !== `/${customThemeConfig.projectName}/` && (
+          <DoczFeedbackForm
+            formActionUrl="https://docs.google.com/forms/u/0/d/e/1FAIpQLScMRAM6r__I_rSeg_l-a5mk8p8iSTDNrjJCUOTlnxa63Czwrg/formResponse"
+            siteName={customThemeConfig.projectName}
+            pageUrl={window.location.href}
+            yesNoFieldName="entry.681938809"
+            feedbackFieldName="entry.593370879"
+            siteFieldName="entry.1824430892"
+            pageUrlFieldName="entry.2081371977"
+          />
+        )}
       <DoczFooter
         authorName={customThemeConfig.footer.authorName}
         authorUrl={customThemeConfig.footer.authorUrl}
