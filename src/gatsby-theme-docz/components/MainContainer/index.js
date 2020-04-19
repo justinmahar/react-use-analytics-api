7ac220b647d3aa182cf87844a28e24f2579dfaf2
Dev Boldly <devboldly@gmail.com>
2020-04-19 18:17:01 -0500

    Add feedback form

--- src/gatsby-theme-docz/components/MainContainer/index.js
+++ src/gatsby-theme-docz/components/MainContainer/index.js
@@ -1,4 +1,4 @@
-import { DoczFooter } from '@devboldly/react-devboldly-tools';
+import { DoczFooter, DoczFeedbackForm } from '@devboldly/react-devboldly-tools';
 import { MainContainer as OriginalMainContainer } from 'gatsby-theme-docz/src/components/MainContainer/index';
 import React from 'react';
 import { customThemeConfig } from '../../custom-theme-config';
@@ -7,6 +7,15 @@ export const MainContainer = ({ children, ...rest }) => {
   return (
     <OriginalMainContainer {...rest}>
       {children}
+      <DoczFeedbackForm
+        formActionUrl="https://docs.google.com/forms/u/0/d/e/1FAIpQLScMRAM6r__I_rSeg_l-a5mk8p8iSTDNrjJCUOTlnxa63Czwrg/formResponse"
+        siteName={customThemeConfig.projectName}
+        pageUrl={window.location.href}
+        yesNoFieldName="entry.681938809"
+        feedbackFieldName="entry.593370879"
+        siteFieldName="entry.1824430892"
+        pageUrlFieldName="entry.2081371977"
+      />
       <DoczFooter
         authorName={customThemeConfig.footer.authorName}
         authorUrl={customThemeConfig.footer.authorUrl}
--- src/gatsby-theme-docz/custom-theme-config.js
+++ src/gatsby-theme-docz/custom-theme-config.js
@@ -1,6 +1,9 @@
+const projectName = 'react-use-analytics-api';
+
 export const customThemeConfig = {
   gaTrackingId: 'UA-148090679-5',
-  faviconUrl: '/react-use-analytics-api/public/favicon.ico',
+  projectName: projectName,
+  faviconUrl: `/${projectName}/public/favicon.ico`,
   footer: {
     authorName: 'DevBoldly',
     authorUrl: 'https://devboldly.com/',
