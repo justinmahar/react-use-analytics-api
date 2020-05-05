2cb65d6ba6578a3422564007df75fc1a601338e8
Dev Boldly <devboldly@gmail.com>
2020-05-05 16:45:15 -0500

    Update Client ID

--- src/__docz__/custom-config.ts
+++ src/__docz__/custom-config.ts
@@ -1,3 +1,3 @@
 export const customConfig = {
-  clientId: '932669268667-ptpd9g2buqb0qv56rk8ki8cpavedku83.apps.googleusercontent.com',
+  clientId: '843919432283-5qntd4ud7lid735vherta2vcgtp96u4r.apps.googleusercontent.com',
 };
